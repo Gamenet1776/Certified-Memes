d71a193981a91d004cae93c48272f79caa45bcfe
Gamenet1776 <143832472+Gamenet1776@users.noreply.github.com>
2024-02-13 09:50:49 -0800

    getting better

--- meme.js
+++ meme.js
@@ -1,3 +1,5 @@
+
+
 // Add Event listners when DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -11,13 +13,13 @@ document.addEventListener("DOMContentLoaded", function () {
     // tbd
     document.getElementById('name').addEventListener('change', function (eventData) {
         let nameOutput = document.getElementById('nameOutput');
-        // console.log(eventData);
+        console.log('name');// console.log(eventData);
         nameOutput.innerText = eventData.target.value;
     });
 
     document.getElementById('type').addEventListener('change', function (eventData) {
         let nameOutput = document.getElementById('nameOutput');
-        // console.log(eventData);
+        console.log('type');// console.log(eventData);
         let info = document.getElementById('info');
         info.innerText = eventData.target.value;
         changeCert(eventData.target.value);
@@ -89,3 +91,31 @@ document.getElementById('certificate-form').addEventListener('submit', function(
     document.getElementById('certification-issuer').textContent = '' + certification.charAt(0).toUpperCase() + certification.slice(1);
     document.getElementById('date').textContent = new Date().toLocaleDateString();
 });
+
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '40px monotype corsiva'
+	ctx.fillStyle = '#29e'
+	ctx.fillText(nameInput.value, 40, 180)
+}
+
+var nameInput = document.getElementById('name2')
+
+function drawImage() {
+	// ctx.clearRect(0, 0, canvas.width, canvas.height)
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+	ctx.font = '40px monotype corsiva'
+	ctx.fillStyle = '#29e'
+	ctx.fillText(nameInput.value, 40, 180)
+}
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const img = document.getElementById("scream");
+ctx.drawImage(img, 10, 10);
+
+ctx.strokeStyle = "white";
